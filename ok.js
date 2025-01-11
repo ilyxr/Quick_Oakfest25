b5d3e738e80b65dec681a7ce0dc2ccb1459490dd
PranadNavada <143791103+PranadNavada@users.noreply.github.com>
2025-01-11 10:31:59 +0530

    Add files via upload

---
+++ ok.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './ok.css;'
+
+const Main = () => {
+  return (
+    <div>
+      <h1 className="title">Placeholder</h1>
+    </div>
+  );
+};
