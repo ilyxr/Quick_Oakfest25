3fea7c80af77316e03f76fcb67fd947069711cea
Quick193 <123229663+Quick193@users.noreply.github.com>
2025-01-11 14:06:37 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.css';
 
 const Main = () => {
+    const [value, setValue] = useState('');
+    const [width, setWidth] = useState(10);
+
+    const onChange = event => {
+        let currentWidth;
+        //if text is being deleted
+        if(value && (value.length > event.target.value.length)){
+            currentWidth = event.target.scrollWidth - 5;
+        } else {
+            currentWidth = event.target.scrollWidth + 1;
+        }
+        setValue(event.target.value);      
+        setWidth(currentWidth);
+    };
+
     return (
         <div className="main-container">
             <div className="title">
@@ -9,7 +24,22 @@ const Main = () => {
             </div>
             
             <div className='input-container'>
-                <input type="text" placeholder='Enter the website URL here' className='text-box' />
+                <input
+                    type="text"
+                    style={{
+                        padding: '0.1cm',
+                        maxWidth: '30cm',
+                        minWidth: '10cm',
+                        width: value ? `${width}px` : '10cm'
+                    }}
+                    value={value}
+                    onChange={onChange}
+                    placeholder='Enter the website URL here'
+                    className='text-box'
+                />
+            </div>
+            <div className='button-container'>
+                <button className='submit-button'>Analyze</button>
             </div>
         </div>
     );
